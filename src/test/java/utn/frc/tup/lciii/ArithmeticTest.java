1efb2612d85c4bd66462845e338470b7c65690b0
14473HJM <hernanjesusmorais@hotmail.com>
2023-02-09 20:13:38 -0300

    Update ArithmeticTest.java

--- src/test/java/utn/frc/tup/lciii/ArithmeticTest.java
+++ src/test/java/utn/frc/tup/lciii/ArithmeticTest.java
@@ -19,17 +19,33 @@ class ArithmeticTest {
 
     @Test
     void subtraction() {
+        BigDecimal testNumber2 = BigDecimal.valueOf(2);
+        BigDecimal expected = BigDecimal.ZERO;
+        BigDecimal result = arithmetic.subtraction(testNumber2, testNumber2);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     void multiplication() {
+        BigDecimal testNumber2 = BigDecimal.valueOf(2);
+        BigDecimal expected = BigDecimal.valueOf(4);
+        BigDecimal result = arithmetic.multiplication(testNumber2, testNumber2);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     void division() {
+        BigDecimal testNumber2 = BigDecimal.valueOf(2);
+        BigDecimal expected = BigDecimal.valueOf(1);
+        BigDecimal result = arithmetic.addition(testNumber2, testNumber2);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     void exponentiation() {
+        BigDecimal testNumber2 = BigDecimal.valueOf(2);
+        BigDecimal expected = BigDecimal.valueOf(4);
+        BigDecimal result = arithmetic.addition(testNumber2, testNumber2);
+        Assertions.assertEquals(expected, result);
     }
 }
